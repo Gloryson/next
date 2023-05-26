1a8431d498c41b4f9fbf472c06c96a78c8fc6180
Vlad Bolkunov <gloryson@mail.ru>
2023-05-26 21:58:42 +0200

    refactor: changed title

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -8,9 +8,9 @@ export function Main () {
   return(
     <section className={'main'}>
       <div className={'main__title'}>
-        <h1>WEB<br/>DEVELOPER<br/>WLADEK</h1>
+        <div className={'title__image'}></div>
       </div>
-      <div className={'main__second'}>
+      <div className={'main__canvas'}>
         <Main3DCanvas />
       </div>
     </section>
